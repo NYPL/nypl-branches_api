e03baf70e1271c6497a799e520926428c8907e49
Sean Redmond <github-smr@sneakemail.com>
2013-11-21 20:39:40 -0500

    Method to return object as resource

--- lib/nypl/branches_api/site.rb
+++ lib/nypl/branches_api/site.rb
@@ -1,6 +1,20 @@
 module Nypl
   module BranchesApi
     class Site < Sequel::Model(:locations_spaces)
+
+      def as_resource
+        { 
+          :id => sid,
+          :name => name,
+          :address => address,
+          :city => city,
+          :zipcode => zipcode,
+          :cross_street => xstreet,
+          :lat => latitude,
+          :long => longitude
+        }
+
+      end
     end
   end
 end
